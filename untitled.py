e9c50a03d981f41070a219ce99c013f616872dad
JOEL_NG <167538526+JOELNGFENG@users.noreply.github.com>
2025-02-19 13:39:11 -0400

    Update untitled.py

--- untitled.py
+++ untitled.py
@@ -1,11 +1,16 @@
 import random
 words = ["bowl","aaa","three"]
+shuffled = words.copy()
+counter = 0
 
 while True:
-    if(words):
-        random_word = random.choice(words)
+    if(shuffled):
+        random_word = random.choice(shuffled)
         input = input("enter random word: ")
         if(random_word == input):
-            
+            print("correct")
+            counter = counter + 1
+        else:
+            print("wrong")
     else:
         break
