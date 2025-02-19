99bb88c497b58bccef23a0cfeb07e66401fa55de
JOEL_NG <167538526+JOELNGFENG@users.noreply.github.com>
2025-02-19 13:37:45 -0400

    Update untitled.py

--- untitled.py
+++ untitled.py
@@ -4,5 +4,8 @@ words = ["bowl","aaa","three"]
 while True:
     if(words):
         random_word = random.choice(words)
+        input = input("enter random word: ")
+        if(random_word == input):
+            
     else:
         break
