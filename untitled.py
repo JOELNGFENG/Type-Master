0377339e03e91fa2dfdb6f8e2aef5fafae52b830
JOEL_NG <167538526+JOELNGFENG@users.noreply.github.com>
2025-02-19 13:42:01 -0400

    Update untitled.py

--- untitled.py
+++ untitled.py
@@ -1,5 +1,6 @@
 import random
-words = ["bowl","aaa","three"]
+words = ["apple", "banana", "cherry", "dog", "elephant", "flower", "grape", "house", "island", "jungle",
+             "keyboard", "lemon", "mountain", "notebook", "orange", "pencil", "queen", "rainbow", "sunshine", "tiger"]
 shuffled = words.copy()
 counter = 0
 
@@ -12,5 +13,8 @@ while True:
             counter = counter + 1
         else:
             print("wrong")
+        
+        shuffled.remove(random_word)
     else:
+        print(counter + " out of " + len(words))
         break
